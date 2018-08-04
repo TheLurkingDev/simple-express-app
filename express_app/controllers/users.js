e3eab67344df8eff4025a8ad03fb6d056859f5d2
TheLurkingDev <thelurkingdev@gmail.com>
2018-08-04 16:30:46 -0400

    Adding a users view.

--- express_app/controllers/users.js
+++ express_app/controllers/users.js
@@ -1,6 +1,7 @@
 module.exports.controller = (app) => {
     // Get users page
     app.get('/users', (req, res) => {
-        res.render('index', { title: 'Users'});
+        res.render('users', { title: 'Users',
+        description: 'This is the description of all the users.'});
     });
 };
